8f4fc474ed2f83938cbdccda989b4aec822db0c5
Itrio <15737258+itsitrio@users.noreply.github.com>
2024-04-26 16:20:25 -0700

    change minutes to color

--- js/timecode.js
+++ js/timecode.js
@@ -3,10 +3,18 @@ const ctx = canvas.getContext('2d');
 canvas.width = 320;
 canvas.height = 40;
 
+// Colors for drawing the colors
+const = colors = ['red','green','blue','cyan','yellow','magenta','black','white']
+
 function drawBlock(value, max, x, width) {
     const luminance = Math.floor((255 - (value / max) * 255));
     const color = `rgb(${luminance},${luminance},${luminance})`;
     ctx.fillStyle = color;
+    ctx.fillRect(x, 0, width, canvas.height);
+}
+function drawColorBlock(minute,x,width) {
+    const colorIndex = minute % colors.length;
+    ctx.fillStyle = colors[colorIndex]
     ctx.fillRect(x, 0, width, canvas.height);
 }
 
@@ -31,7 +39,7 @@ function updateTime() {
     // Draw time blocks
     drawBlock(days, 6, 0, 40); // Hours
     drawBlock(hours, 23, 40, 40); // Hours
-    drawBlock(minutes, 59, 80, 40); // Minutes
+    drawColorBlock(minutes, 59, 80, 40); // Minutes
     drawBlock(seconds, 59, 120, 40); // Seconds
     drawBlock(deciseconds, 9, 160, 40); // Upper Deciseconds
     drawBlock(centiseconds, 9, 200, 40); // Lower Centiseconds
